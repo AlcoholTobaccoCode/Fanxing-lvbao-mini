35b6acb64903e517006aad5f77e425631821d0a9
duqings <duqings@foxmail.com>
2025-11-17 18:12:58 +0800

    feat(iconfont): ✨ 自定义 iconfont

---
+++ icons/fxzxiconfont/index.ts
@@ -0,0 +1,10 @@
+export const fxzxiconfont = {
+	"fxzh-dakaijianpan": "e619",
+	"fxzh-hulianwang": "e607",
+	"fxzh-jianpan": "e61c",
+	"fxzh-wangzhan": "e6f5",
+	"fxzh-jianpan1": "e65f",
+	"fxzh-jianpan2": "ed15",
+	"fxzh-zhishiku10": "ea4e",
+	"fxzh-zhishiku": "e60a"
+};
--- icons/index.ts
+++ icons/index.ts
@@ -1,7 +1,9 @@
+import { fxzxiconfont } from "./fxzxiconfont";
 import { iconfont } from "./iconfont";
 import { remixicon } from "./remixicon";
 
 export const icons = {
+	fxzxiconfont,
 	iconfont,
 	remixicon
 };
