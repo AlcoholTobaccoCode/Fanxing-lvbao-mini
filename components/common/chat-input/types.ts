ac1a4f551e7db9d5307f947a0e5133105b61c0e1
duqings <duqings@foxmail.com>
2025-11-17 18:13:17 +0800

    feat(底部聊天组件): ✨ 基础

---
+++ components/common/chat-input/types.ts
@@ -0,0 +1,21 @@
+export type ToolItem = {
+	icon: string;
+	text: string;
+	enable: boolean;
+	onClick: (item: ToolItem) => void;
+};
+
+export type Tools = ToolItem[];
+
+export type ActionItem = {
+	icon: string;
+	text: string;
+	disabled: boolean;
+	onClick: (item: ActionItem) => void;
+};
+
+export type Actions = ActionItem[];
+
+export type SendData = {
+	text: string;
+};
