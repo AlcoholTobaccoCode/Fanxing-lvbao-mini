70de0eec825b8988edc1aa9922f243d6fc430e19
duqings <duqings@foxmail.com>
2025-12-22 15:55:09 +0800

    perf: ⚡ 底部聊天窗口优化交互、持续更新 icon

--- icons/fxzxiconfont/index.ts
+++ icons/fxzxiconfont/index.ts
@@ -1,4 +1,7 @@
 export const fxzxiconfont = {
+	"fxzh-saying": "e62f",
+	"fxzh-yuyin": "e6f6",
+	"fxzh-yuyin1": "e66c",
 	"fxzh-dakaijianpan": "e619",
 	"fxzh-hulianwang": "e607",
 	"fxzh-jianpan": "e61c",
