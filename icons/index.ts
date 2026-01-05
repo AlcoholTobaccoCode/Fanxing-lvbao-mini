0bce4465511bb1a2bed8406ab9cb6501ccb7af92
duqings <duqings@foxmail.com>
2026-01-05 18:04:58 +0800

    feat(icon): ✨ 新增编辑器 icon

--- icons/index.ts
+++ icons/index.ts
@@ -1,8 +1,10 @@
+import { editicon } from "./editicon";
 import { fxzxiconfont } from "./fxzxiconfont";
 import { iconfont } from "./iconfont";
 import { remixicon } from "./remixicon";
 
 export const icons = {
+	editicon,
 	fxzxiconfont,
 	iconfont,
 	remixicon
