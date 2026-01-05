b76e40d149cc793445f715ad592f74fccc0ba4ee
duqings <duqings@foxmail.com>
2026-01-05 18:05:31 +0800

    feat: ✨ 新增富文本编辑器

---
+++ components/sv-editor/components/common/tool-list.js
@@ -0,0 +1,208 @@
+/**
+ * 工具栏
+ * @author sonve
+ * @version 1.0.0
+ * @date 2024-12-04
+ */
+
+export const toolList = [
+	{ title: "样式", name: "style", icon: "edit-zitiyanse" },
+	{ title: "表情", name: "emoji", icon: "edit-xiaolian" },
+	{ title: "撤销", name: "undo", icon: "edit-shangyibu" },
+	{ title: "重做", name: "redo", icon: "edit-xiayibu" },
+	{ title: "更多", name: "more", icon: "edit-tianjia" },
+	{ title: "扩展", name: "setting", icon: "edit-setting" }
+];
+
+export const styleToolList = [
+	{ title: "标题", name: "header", value: 2, icon: "edit-zitibiaoti" },
+	{ title: "分割线", name: "divider", icon: "edit-fengexian" },
+	{ title: "粗体", name: "bold", icon: "edit-zitijiacu" },
+	{ title: "斜体", name: "italic", icon: "edit-zitixieti" },
+	{ title: "下划线", name: "underline", icon: "edit-zitixiahuaxian" },
+	{ title: "删除线", name: "strike", icon: "edit-zitishanchuxian" },
+	{ title: "左对齐", name: "align", value: "left", icon: "edit-zuoduiqi" },
+	{ title: "居中", name: "align", value: "center", icon: "edit-juzhongduiqi" },
+	{ title: "右对齐", name: "align", value: "right", icon: "edit-youduiqi" },
+	{ title: "有序列表", name: "list", value: "ordered", icon: "edit-youxupailie" },
+	{ title: "无序列表", name: "list", value: "bullet", icon: "edit-wuxupailie" },
+	{ title: "上标", name: "script", value: "super", icon: "edit-zitishangbiao" },
+	{ title: "左缩进", name: "indent", value: "+1", icon: "edit-zuosuojin" },
+	{ title: "右缩进", name: "indent", value: "-1", icon: "edit-yousuojin" },
+	{ title: "下标", name: "script", value: "sub", icon: "edit-zitixiabiao-copy" },
+	{ title: "文字颜色", name: "color", icon: "edit-wenziyanse" },
+	{ title: "背景颜色", name: "backgroundColor", icon: "edit-beijingyanse" },
+	{ title: "清除格式", name: "removeformat", icon: "edit-qingchugeshi" }
+];
+
+export const moreToolList = [
+	{ title: "添加图片", name: "image", value: "popup", icon: "edit-charutupian" },
+	{ title: "添加视频", name: "video", value: "popup", icon: "edit-video" },
+	{ title: "添加链接", name: "link", value: "popup", icon: "edit-charulianjie" },
+	{ title: "添加附件", name: "attachment", value: "popup", icon: "edit-huixingzhen" },
+	{ title: "提及", name: "at", value: "popup", icon: "edit-at" },
+	{ title: "话题", name: "topic", value: "popup", icon: "edit-huati" },
+	{ title: "清空", name: "clear", value: "button", icon: "edit-shanchu" }
+];
+
+export const emojiToolList = [
+	"😊", // 笑笑
+	"😃", // 大笑
+	"😄", // 开心果
+	"😁", // 嘲讽
+	"😆", // 爆笑
+	"😅", // 出汗笑
+	"🤣", // 滚地大笑
+	"😂", // 泪流满面
+	"🙂", // 轻松愉快
+	"🙃", // 上下翻白眼
+	"😉", // 鬼鬼祟祟
+	"😌", // 安慰
+	"😍", // 心动
+	"🥰", // 深情
+	"😘", // 吻
+	"😗", // 接吻
+	"😙", // 亲吻
+	"😚", // 亲吻
+	"😋", // 哇塞
+	"😛", // 舌头外伸
+	"😝", // 舌头吐出
+	"😜", // 顽皮
+	"🤪", // 疯狂
+	"😎", // 自豪
+	"🤓", // 学究
+	"🧐", // 思考
+	"😏", // 狡猾
+	"😒", // 不高兴
+	"😞", // 不开心
+	"😔", // 抒发情绪
+	"😟", // 担忧
+	"😕", // 困惑
+	"🙁", // 小失望
+	"☹️️", // 不好意思
+	"😣", // 苦恼
+	"😖", // 愤怒
+	"😫", // 累
+	"😩", // 悲伤
+	"😤", // 生气
+	"😠", // 生气
+	"😡", // 极端愤怒
+	"🤬", // 发飙
+	"🤯", // 爆炸头脑
+	"😳", // 吃惊
+	"😱", // 惊吓
+	"😨", // 恐惧
+	"😰", // 慌张
+	"😢", // 哭泣
+	"😭", // 大哭
+	"😓", // 受挫
+	"🤗", // 给力
+	"🤔", // 思考
+	"🤭", // 戴口罩捂嘴笑
+	"🤫", // 戴口罩做鬼脸
+	"🤥", // 说谎
+	"😬", // 格格不入
+	"😴", // 睡觉
+	"🤤", // 垂涎欲滴
+	"🥳", // 庆祝
+	"🥺", // 求求你
+	"😈", // 恶魔
+	"👿", // 恶灵
+	"🤡", // 小丑
+	"👻", // 鬼魂
+	"👽", // 外星人
+	"👾", // 游戏角色
+	"🤖", // 机器人
+	"😺", // 笑猫
+	"😸", // 大笑猫
+	"😹", // 开心猫
+	"😻", // 心动猫
+	"😼", // 傲娇猫
+	"😽", // 亲吻猫
+	"🙀", // 惊吓猫
+	"😿", // 哭猫
+	"😾" // 生气猫
+];
+
+export const colorList = [
+	"#000000",
+	"#222222",
+	"#444444",
+	"#666666",
+	"#999999",
+	"#cccccc",
+	"#eeeeee",
+	"#ffffff",
+
+	"#c92a2a",
+	"#e03131",
+	"#f03e3e",
+	"#fa5252",
+	"#ff6b6b",
+	"#ff8787",
+	"#ffa8a8",
+	"#ffc9c9",
+
+	"#a61e4d",
+	"#c2255c",
+	"#d6336c",
+	"#e64980",
+	"#f06595",
+	"#f783ac",
+	"#faa2c1",
+	"#fcc2d7",
+
+	"#862e9c",
+	"#9c36b5",
+	"#ae3ec9",
+	"#be4bdb",
+	"#cc5de8",
+	"#da77f2",
+	"#e599f7",
+	"#eebefa",
+
+	"#5f3dc4",
+	"#6741d9",
+	"#7048e8",
+	"#7950f2",
+	"#845ef7",
+	"#9775fa",
+	"#b197fc",
+	"#d0bfff",
+
+	"#0b7285",
+	"#0c8599",
+	"#1098ad",
+	"#15aabf",
+	"#22b8cf",
+	"#3bc9db",
+	"#66d9e8",
+	"#99e9f2",
+
+	"#087f5b",
+	"#099268",
+	"#0ca678",
+	"#12b886",
+	"#20c997",
+	"#38d9a9",
+	"#63e6be",
+	"#96f2d7",
+
+	"#5c940d",
+	"#66a80f",
+	"#74b816",
+	"#82c91e",
+	"#94d82d",
+	"#a9e34b",
+	"#c0eb75",
+	"#ffec99",
+
+	"#d9480f",
+	"#e8590c",
+	"#f76707",
+	"#fd7e14",
+	"#ff922b",
+	"#ffa94d",
+	"#ffc078",
+	"#ffd8a8"
+];
